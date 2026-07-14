e06ea002b12cb2b25707f9c4222d632acd37aa03
Awais Ali <awaisalidev@gmail.com>
2026-07-14 22:05:13 +0500

    doing stuff

--- chapter_01/src/Game.cpp
+++ chapter_01/src/Game.cpp
@@ -5,34 +5,49 @@ SDL_Renderer *g_pRenderer = 0;
 
 bool is_game_running = false;
 
-extern DECLSPEC SDL_Window *SDLCALL SDL_CreateWindow(const char *title, int x,
-                                                     int y, int w, int h,
-                                                     Uint32 flags);
-bool init )
+void init();
+void handle_events();
+void render();
+void clean();
+void update();
 
-
-int main(int argc, char *argv[]) {
+bool init(const char *title, int x, int y, int w, int h, int flags) {
 
   // Init SDL
 
-  if (SDL_Init(SDL_INIT_EVERYTHING) > 0) {
+  if (SDL_Init(SDL_INIT_EVERYTHING) == 0) {
 
     // If succeeded create our winwow
 
-    g_pWindow = SDL_CreateWindow(
-        "Chatper 1: Setting up SDL", SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED, 800, 600,
-        SDL_WINDOW_SHOWN); // If window not created show an error message
-    if (g_pWindow != 0) {
+    g_pWindow = SDL_CreateWindow(title, x, y, w, h, flags);
+
+    if (g_pWindow) {
+
       g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
+
+      if (g_pRenderer) {
+
+        return true;
+      } else {
+
+        SDL_Log("SDL Window creation failed: %s", SDL_GetError());
+        return false;
+      }
+
+    } else {
+
+      SDL_Log("SDL Window creation failed: %s", SDL_GetError());
+      return false;
     }
 
   } else {
 
-    return -1;
+    SDL_Log("SDL Window creation failed: %s", SDL_GetError());
+    return false;
   }
+}
 
-  // Everything succeeded
+void render() {
 
   SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
 
@@ -40,7 +55,37 @@ int main(int argc, char *argv[]) {
 
   SDL_RenderPresent(g_pRenderer);
 
-  SDL_Delay(15000);
+  SDL_Delay(15);
+}
+
+int main(int argc, char *argv[]) {
+
+  init();
+
+  while (is_game_running) {
+
+    handle_events();
+    update();
+    render();
+  }
+
+  clean();
+
+  if (init("SDL Game Development", SDL_WINDOWPOS_CENTERED,
+           SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_SHOWN)) {
+
+    is_game_running = true;
+  }
+
+  else {
+
+    return 1;
+  }
+
+  while (is_game_running) {
+
+    render();
+  }
 
   SDL_Quit();
 }
